49fb9deec31fce2e971a0b0a1de59f24d3f23bfa
Donald Whitely <dswhitely1@gmail.com>
2019-07-16 18:17:29 -0400

    Setting InitialValue

--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -2,7 +2,7 @@ import {useEffect} from 'react'
 import {useLocalStorage} from './useLocalStorage';
 
 export const useDarkMode = () => {
-  const [darkMode, setDarkMode] = useLocalStorage('dark_mode');
+  const [darkMode, setDarkMode] = useLocalStorage('dark_mode',false);
 
   useEffect(()=> {
     const body = document.querySelector('body');
